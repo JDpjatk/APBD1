2284ebb79df5f3c2c10d56add5d27a9e19ff4a2b
Jakub Daniuk <s27365@pjwstk.edu.pl>
2025-03-02 11:25:08 +0100

    static method

--- APBD1/APBD1/Program.cs
+++ APBD1/APBD1/Program.cs
@@ -1,3 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("First Change!");
+Console.WriteLine(test([1,3,5,2137]));
+static double test(int[] args)
+{ 
+    int sum = 0;
+    foreach(int i in args) {  sum += i; }
+
+    return sum / args.Length;
+    Console.WriteLine("test");
+}
