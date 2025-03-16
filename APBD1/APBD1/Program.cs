ed51a94d260541d989ba81d257fde2df64858cf3
Jakub Daniuk <s27365@pjwstk.edu.pl>
2025-03-16 09:11:45 +0100

    Create max

--- APBD1/APBD1/Program.cs
+++ APBD1/APBD1/Program.cs
@@ -1,10 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine(
-test([1,3,5,2,4,14,5,5,2,2]));
-static double test(int[] args)
+avarage([1,3,5,2,4,14,5,5,2,2]));
+Console.WriteLine(max([1,3,5,2,4,14,5,5,2,2]));
+static double avarage(int[] args)
 { 
     int sum = 0;
     foreach(int i in args) {  sum += i; }
 
     return sum / args.Length;
+}
+
+static int max(int[] args){
+    int max = args[0];
+    foreach(int i in args){
+        if (i > max)
+        {
+            max = i;
+        }
+    }return max;
+
 }
