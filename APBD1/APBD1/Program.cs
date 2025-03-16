43aa30600ca13dc961470ea30e3fcec8c5243b58
Jakub Daniuk <s27365@pjwstk.edu.pl>
2025-03-16 09:00:03 +0100

    fixed avarage

--- APBD1/APBD1/Program.cs
+++ APBD1/APBD1/Program.cs
@@ -1,12 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("First Change!");
-Console.WriteLine(test([1,3,5,2137]));
+Console.WriteLine(
+test([1,3,5,2,4,14,5,5,2,2]));
 static double test(int[] args)
 { 
     int sum = 0;
     foreach(int i in args) {  sum += i; }
 
     return sum / args.Length;
-    Console.WriteLine("test");
 }
